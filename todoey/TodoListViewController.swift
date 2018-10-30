87744e001f37e55c86f6bec538ed19e56ca014e2
VishalThakkar <vishalthakkar@vts-mac-mini.local>
2018-10-30 13:27:24 +0530

    Data done with userdefaults

--- todoey/TodoListViewController.swift
+++ todoey/TodoListViewController.swift
@@ -11,11 +11,16 @@ class TodoListViewController: UITableViewController {
 
   var itemArray = ["find Mike","Buy Eggos","Destroy Demogorgon"]
     
+    let defaults = UserDefaults.standard
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-  
+     if let items =  defaults.array(forKey: "TodoListArray") as? [String]
+     {
+        itemArray = items
+        
+    }
     }
 
 
@@ -57,6 +62,8 @@ class TodoListViewController: UITableViewController {
         let action = UIAlertAction(title:  "Add Item", style: .default) { (action) in
         
             self.itemArray.append(textField.text!)
+            self.defaults.set(self.itemArray, forKey: "TodoListArray")
+            
             self.tableView.reloadData()
             
     }
